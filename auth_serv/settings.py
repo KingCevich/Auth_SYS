c9061fdf29e2b2336754cd0acb82252f33fcac53
NACV <na.cifva7@gmail.comm>
2026-07-12 15:52:31 -0400

    configuraciones prod

--- auth_serv/settings.py
+++ auth_serv/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5hg^o8$c=1x2e^5&^bwi!m^#c%wwq0blw8=t!%2#26=$m_-o8z'
+ECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-5hg^o8$c=1x2e^5&^bwi!m^#c%wwq0blw8=t!%2#26=$m_-o8z')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
@@ -89,16 +91,10 @@ WSGI_APPLICATION = 'auth_serv.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "auth_db",
-        "USER": "auth_user",
-        "PASSWORD": "pass123",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
+    'default': dj_database_url.parse(
+        os.environ.get('DATABASE_URL', 'postgresql://auth_user:pass123@localhost:5432/auth_db')
+    )
 }
-
 
 
 # Password validation
@@ -136,3 +132,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
